e2508c5ede6264189b80f96b8123179c08e8b732
Mohamed91128 <mohamedhisham10199@gmail.com>
2025-10-24 20:52:44 +0400

    Add files via upload

--- gamelua/3527290.lua
+++ gamelua/3527290.lua
@@ -1,3 +1,12 @@
 addappid(3527290)
 addappid(3527291,0,"f55585bb6a00ba332421256f80ba05e27afc38fec6170b9a763138244c506f05")
-setManifestid(3527291,"548744499804961861")
+setManifestid(3527291,"1786827858954525980")
+
+
+
+
+
+--[[
+This file belongs to Ahmeds Bot Discord Server
+-----> join discord Server : https://discord.gg/sv6EGxCRnC
+]]
