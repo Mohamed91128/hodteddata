10d7614da1298871104557675037fe1e59e231fe
Mohamed91128 <mohamedhisham10199@gmail.com>
2026-01-09 01:12:17 +0400

    Add files via upload

--- gamelua/1903340.lua
+++ gamelua/1903340.lua
@@ -1,12 +1,10 @@
+-- This lua file has been fetched from the bot VICTOR which is the exclusive property of Piracy Lords server.
+-- Redistribution of VICTORs files is not allowed AT ALL
+-- Join the offical here: https://discord.gg/piracylords
+
 addappid(1903340)
-addappid(3228520)
-addappid(1903341,0,"8593f9c4cd595f833d5344e55cd6c6fdb73d9a4fa05dab033ba71622265e8c82")
-setManifestid(1903341,"5318062265348832578")
+addappid(1903341, 1, "8593f9c4cd595f833d5344e55cd6c6fdb73d9a4fa05dab033ba71622265e8c82")
+--setManifestid(1903341, "3291917955150102469", 43614197058)
 
-
-
-
---[[
-This file belongs to Ahmeds Bot Discord Server
------> join discord Server : https://discord.gg/sv6EGxCRnC
-]]
+-- DLCs without dedicated depots
+addappid(3228520) -- Clair Obscur Expedition 33  Deluxe Edition Upgrade
