35820ccdd6953babbb82d1d0e18ac1a5bfadb102
Mohamed91128 <mohamedhisham10199@gmail.com>
2026-02-17 19:22:57 +0400

    Add files via upload

---
+++ gamelua/2486820.lua
@@ -0,0 +1,13 @@
+-- ============================================
+--  Credits: K3rnelPan1c (KernelOS / !K3rnalyze)
+--  Discord: https://discord.gg/k3rnalyze
+--  Website: https://kernelos.org/games
+-- ============================================
+
+addappid(2486820)
+addappid(2486821,0,"08e2539b55bc79fefa9447d3479c57b481c4256c12ebd50529fc3722fcc3727f")
+addappid(3565190)
+addappid(3592250)
+addappid(3601500)
+addappid(3601510)
+addappid(3601530)
